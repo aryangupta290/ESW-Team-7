d4b0395116377f33d4c5ac125ea825fa2b8c960d
Aryan Gupta <aryan.gupta@students.iiit.ac.in>
2022-09-26 19:15:05 +0530

    Update and rename secrets (3).h to secrets.h

---
+++ secrets.h
@@ -0,0 +1,14 @@
+// Use this file to store all of the private credentials 
+// and connection details
+#define SECRET_SSID ""    // replace MySSID with your WiFi network name
+#define SECRET_PASS "" // replace MyPassword with your WiFi password
+
+
+#define SECRET_CH_ID ""			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY ""   // replace XYZ with your channel write API Key
+#define SECRET_OM2M_ID ""
+
+
+//other-channel
+#define CH_ID ""      // replace 0000000 with your channel number
+#define WRITE_APIKEY ""   // replace XYZ with your channel write API Key
